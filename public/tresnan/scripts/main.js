9eefb8d102b4a804c68ac9dab4ad303de6eb9894
saladinid <didien.geonk@gmail.com>
2020-05-04 17:11:53 +0800

    form ok

--- public/tresnan/scripts/main.js
+++ public/tresnan/scripts/main.js
@@ -1,3 +1,22 @@
 function closeMenu() {
     document.getElementById("menu-btn").checked = false;
 }
+
+function goForm() {
+    document.getElementById("block-form").style.display = "block";
+    document.getElementById("block-waiting").style.display = "none";
+    document.getElementById("block-result").style.display = "none";
+}
+
+goForm();
+
+function goCheck() {
+    document.getElementById("block-form").style.display = "none";
+    document.getElementById("block-waiting").style.display = "block";
+    document.getElementById("block-result").style.display = "none";
+    setTimeout(function () {
+        document.getElementById("block-form").style.display = "none";
+        document.getElementById("block-waiting").style.display = "none";
+        document.getElementById("block-result").style.display = "block";
+    }, 3000);
+}
